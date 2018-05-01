c51d500a6d5f1a804715492fc3df9d4273102377
Josh Hodgson <josh.pbh@gmail.com>
2018-05-01 22:42:46 +0100

    formatting and fixing

--- index.js
+++ index.js
@@ -13,42 +13,50 @@ var qualityIds = [2, 3, 4, 5, 6]
 var publicNotifier = new events.EventEmitter();
 
 
-webhook.on("live", function(payload){
+webhook.on("live", function (payload) {
 
-    
-    new recorder(payload.payload.id)
+    var url
+    website.apiRequest("mediaItems/" + payload.payload.id, function (data) {
+        // console.log(data.data.mediaItem)
+        url = getMediaUrl(data.data.mediaItem, "stream")
+        console.log(url)
+
+        new Recorder(url, payload.payload.id)
+
+    })
 })
 
 
-function recorder(id){
+//webhook.emit("live", {payload:{id: 826}})
+//  ----that was my cheaty test
+
+
+function Recorder(url, id) {
 
     //get URL
-    website.apiRequest("mediaItems/"+id, function(data){
-        // console.log(data.data.mediaItem)
-         console.log(getMediaUrl(data.data.mediaItem, "live").url)
-     })
-
     if(!url){
-        console.error("Oops, can't get a URL for that one")
+        console.error("No url stuff!")
+        return
     }
 
+    var commandArgs = ["-i", url.url, "-c", "copy", "-bsf:a", "aac_adtstoasc ", "/mnt/volume-lon1-01/autorot/" + url.mediaItem.id + "_" + url.mediaItem.name + ".mp4"]
 
-    var commandArgs = ["-i", url.url, "-c","copy","-bsf:a"," aac_adtstoasc ", __dirname + "/" + url.mediaItem + ".mp4"]
-
-        //ffmpeg -i http://.../playlist.m3u8 -c copy -bsf:a aac_adtstoasc output.mp4
-console.log("starting ffmpeg, " + commandArgs)
+    //ffmpeg -i http://.../playlist.m3u8 -c copy -bsf:a aac_adtstoasc output.mp4
+    console.log("starting ffmpeg, " + commandArgs)
     var ffmpeg = spawn("ffmpeg", commandArgs)
 
-    ffmpeg.on('exit', function(){console.log("Exited ")});
-	
+    ffmpeg.on('exit', function () {
+        console.log("Exited ")
+    });
+
 
 
     console.log("end" + id)
-    webhook.once("end" + id, function(){
+    webhook.once("end" + id, function () {
         console.log("Killing FFMPEG id " + id)
         ffmpeg.kill()
     })
-  
+
 
 }
 
@@ -57,31 +65,34 @@ function getMediaUrl(mediaItem, type) {
     if (type !== "video" && type !== "stream") {
         throw "Invalid item type.";
     }
-    
-   
+
+
     var mediaItemPart = type === "video" ? mediaItem.vod : mediaItem.liveStream;
-    
+    if(!mediaItemPart.qualities){
+        console.error("looks like there aren't any sources to pick from...")
+        return null;
+    }
+
     var availableQualityIds = [];
-    for (var j=0; j<mediaItemPart.qualities.length; j++) {
+    for (var j = 0; j < mediaItemPart.qualities.length; j++) {
         availableQualityIds.push(mediaItemPart.qualities[j].id);
     }
     var chosenUrl = null;
-    for (var j=0; j<qualityIds.length && chosenUrl === null; j++) {
+    for (var j = 0; j < qualityIds.length && chosenUrl === null; j++) {
         var proposedQualityId = qualityIds[j];
         if (availableQualityIds.indexOf(proposedQualityId) !== -1) {
             // find the url for the mp4 encoded version
-            for (var k=0; k<mediaItemPart.urlData.length && chosenUrl === null; k++) {
+            for (var k = 0; k < mediaItemPart.urlData.length && chosenUrl === null; k++) {
                 var item = mediaItemPart.urlData[k];
                 if (item.quality.id === proposedQualityId) {
-                    for(var j=0; j<item.urls.length; j++) {
+                    for (var j = 0; j < item.urls.length; j++) {
                         var urlInfo = item.urls[j];
                         if (type === "video") {
                             if (urlInfo.type === "video/mp4") {
                                 chosenUrl = urlInfo.url;
                                 break;
                             }
-                        }
-                        else if (type === "stream") {
+                        } else if (type === "stream") {
                             if (urlInfo.type === "application/x-mpegURL") {
                                 chosenUrl = urlInfo.url;
                                 break;
@@ -96,14 +107,10 @@ function getMediaUrl(mediaItem, type) {
         // could not find an applicable url
         return null;
     }
-    
+
     return {
         mediaItem: mediaItem,
         url: chosenUrl,
         type: type
     };
 }
-
-
-
-
